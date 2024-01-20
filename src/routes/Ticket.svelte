490d8906935a24da8e3ce1f5aec948e25e2fb8b5
Joshua Ofosu <joeokat@gmail.com>
2024-01-20 21:20:57 +0000

    Update Ticket.svelte

--- src/routes/Ticket.svelte
+++ src/routes/Ticket.svelte
@@ -45,7 +45,7 @@
 	  {/if}
 
 	  {#if show}
-      <h3 class="vipOdds">BE8396B9</h3>
+      <h3 class="vipOdds">N/A</h3>
 	  {/if}
     <p class="freeOdds"><strong>Recovery Odd:</strong> BE8396B9</p>
     </div>
