27af5327c8845921455683482a3ea61e575f2fb5
Joshua <joeokat@gmail.com>
2024-01-21 07:49:58 +0000

    HtmlLogic

--- src/routes/SpartanPlus.svelte
+++ src/routes/SpartanPlus.svelte
@@ -1,8 +1,18 @@
 <script>
   let src = '/backgroundImg.png';
 
-  function handleButton() {
-    alert('Stay calm! VIP will be right back.')
+  function handleModal() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('modal').showModal();
+  }
+
+  function handleClose() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('modal').close();
+  }
+
+  function handlePayment() {
+    //
   }
 </script>
 
@@ -14,7 +24,22 @@
       <div>
         Make Your Life Easier With   →
       </div>
-      <button on:click={handleButton}>Spartan +</button>
+      <button on:click={handleModal}>Spartan +</button>
     </div>
   </div>
 </div>
+
+<div class="overlay" id="overlay"></div>
+
+<dialog id="modal" open>
+  <h3>Get VIP Odds</h3>
+  <p>For only Ghs25 daily to access Spartan +</p>
+  <div class="dialog-btns">
+    <button class="btn-cancel" on:click={handleClose}>Cancel</button>
+    <button class="btn-subscribe" on:click={handlePayment}>Subscribe</button>
+  </div>
+</dialog>
+
+<style>
+
+</style>
