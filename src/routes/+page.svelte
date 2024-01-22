31280a20f15495c1716f7e7a1986c27aa8bb1e41
Nii Aryeh <sup@niiaryeh.com>
2024-01-22 21:30:55 +0000

    refactor layout, privacy page updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script>
-  import Nav from "./Nav.svelte";
   import SpartanPlus from "./SpartanPlus.svelte";
   import Terms from "./Terms.svelte";
   import Ticket from "./Ticket.svelte";
 </script>
 
 <main>
-  <Nav />
   <div class="sectionY">
     <SpartanPlus />
     <Ticket />
