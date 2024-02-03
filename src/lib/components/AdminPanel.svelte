5ee37e8d8b1102e9627fc87d616d7708c18b75b9
Joshua <joeokat@gmail.com>
2024-02-03 04:58:56 +0000

    Create AdminPanel.svelte

---
+++ src/lib/components/AdminPanel.svelte
@@ -0,0 +1,31 @@
+<script>
+  let formInput = {
+		email: '',
+		firstName: '',
+		lastName: ''
+	}
+</script>
+
+<div class="subscription-form">
+
+	<form id="adminPanel" on:submit|preventDefault={payWithPaystack}>		
+	  
+		<div class="parent-group">
+		<div class="form-group">
+		  <input type="text" id="first-name" placeholder="First Name" bind:value={formInput.firstName}/>
+		</div>
+	  
+		<div class="form-group">
+		  <input type="text" id="last-name" placeholder="Last Name" bind:value={formInput.lastName}/>
+		</div>
+		</div>
+
+		<div class="form-group">
+			<input type="email" id="email-address" required placeholder="Email Address" bind:value={formInput.email}/>
+		  </div>
+	  
+		<div class="form-submit">
+		  <button type="submit">Subscribe to Spartan+</button>
+		</div>
+	</form>
+</div>
