f511624b4fa3e6a0997eab637cf0d67a58b482ba
Joshua Ofosu <joeokat@gmail.com>
2024-02-18 23:28:24 +0000

    Update PaymentForm.svelte

--- src/lib/components/PaymentForm.svelte
+++ src/lib/components/PaymentForm.svelte
@@ -60,7 +60,7 @@
 		  </div>
 	  
 		<div class="form-submit">
-		  <button type="submit"> Tip to win more </button>
+		  <button type="submit"> Get 3X ticket for only $2 </button>
 		</div>
 	</form>
 </div>
