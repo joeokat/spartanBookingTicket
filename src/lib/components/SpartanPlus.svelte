5ec6c74b788c49adb2c5c6e446b345e848da3f8f
Joshua Ofosu <joeokat@gmail.com>
2024-03-21 18:43:59 +0000

    Update SpartanPlus.svelte

--- src/lib/components/SpartanPlus.svelte
+++ src/lib/components/SpartanPlus.svelte
@@ -1,18 +1,16 @@
-<div class="spartanPlus">
-  <div class="container">
-    <img {src} alt="">
-    <img class="sportyPlayer" alt="" src="/sportyImg.png">
-    <div class="spartanVIP">
-      <div>
-        Subscribe to Spartan+
-      </div>
-      <button on:click={handleModal}> Support us </button>
-    </div>
-  </div>
-</div>
-
 <script>
-  let src = '/backgroundImg.png';
+	let src = '/backgroundImg.png'
 
-  export let handleModal
+	export let handleModal
 </script>
+
+<div class="spartanPlus">
+	<div class="container">
+		<img {src} alt="" />
+		<img class="sportyPlayer" alt="" src="/sportyImg.png" />
+		<div class="spartanVIP">
+			<div>Subscribe to Spartan Plus🍄</div>
+			<button on:click={handleModal}> Support us </button>
+		</div>
+	</div>
+</div>
