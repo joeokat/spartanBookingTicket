04ebae18c3e44110d34f6cee978fd2baf5df642f
Joshua Ofosu <joeokat@gmail.com>
2024-04-01 17:37:51 +0000

    Update Ticket.svelte

--- src/lib/components/Ticket.svelte
+++ src/lib/components/Ticket.svelte
@@ -59,7 +59,7 @@
 			{/if}
 
 			{#if subscribed_value}
-				<h3 class="vipOdds"> B3BACUR 👏🏽 </h3>
+				<h3 class="vipOdds"> B3BQSEG 👏🏽 </h3>
 			{/if}
 			<p style="color: #919191; margin:1rem; text-align: center">
 				subscription is valid until ticket is won
