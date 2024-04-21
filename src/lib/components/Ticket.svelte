a00c565ebded6f28858bce921115f23051ee5b3f
Joshua Ofosu <joeokat@gmail.com>
2024-04-21 10:09:30 +0000

    Update Ticket.svelte

--- src/lib/components/Ticket.svelte
+++ src/lib/components/Ticket.svelte
@@ -52,14 +52,14 @@
 	<div class="barcode">
 		<div class="winning-ticket">
 			<p style="margin:1rem; text-align: center">
-				PariMatch:<br /> <strong> Booking Code: </strong>
+				SportyBet:<br /> <strong> Booking Code: </strong>
 			</p>
 			{#if !subscribed_value}
 				<img {src} alt="" on:click={handleModal} />
 			{/if}
 
 			{#if subscribed_value}
-				<h3 class="vipOdds"> TUT35JXA 👏🏽 </h3>
+				<h3 class="vipOdds"> E3D8EF6 👏🏽 </h3>
 			{/if}
 			<p style="color: #919191; margin:1rem; text-align: center">
 				subscription is valid until ticket is won
