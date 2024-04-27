0419bdc56e6a2409269e397c7180a6647dff6e30
Joshua Ofosu <joeokat@gmail.com>
2024-04-27 09:47:41 +0100

    Update PaymentForm.svelte

--- src/lib/components/PaymentForm.svelte
+++ src/lib/components/PaymentForm.svelte
@@ -17,7 +17,7 @@
 	}
 
 	function payWithPaystack() {
-		const amount = 50.0
+		const amount = 10.0
 
 		let handler = PaystackPop.setup({
 			key: 'pk_live_b2d929e70c0300736d8d7ed9e36b109510f82820',
@@ -73,7 +73,7 @@
 		</div>
 
 		<div class="form-submit">
-			<button type="submit"> Subscribe for ¢50 per month </button>
+			<button type="submit"> Subscribe for today </button>
 		</div>
 	</form>
 </div>
