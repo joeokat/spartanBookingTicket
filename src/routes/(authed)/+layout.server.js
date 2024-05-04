03ac0be99b88dea123e4eae192e49a411c8ff763
Joshua Ofosu <joeokat@gmail.com>
2024-05-04 15:26:40 +0000

    AllGoodNowHopefully

--- src/routes/(authed)/+layout.server.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load({ cookies, url }) {
-	if (!cookies.get('logged_in')) {
-		throw redirect(303, `/login?redirectTo=${url.pathname}`);
-	}
-}
