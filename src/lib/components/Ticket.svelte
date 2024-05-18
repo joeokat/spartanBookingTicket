ac0c205d80c0145bb81d4aa4b832655734faca10
Joshua Ofosu <joeokat@gmail.com>
2024-05-18 08:53:45 +0000

    Update Ticket.svelte

--- src/lib/components/Ticket.svelte
+++ src/lib/components/Ticket.svelte
@@ -52,14 +52,14 @@
 	<div class="barcode">
 		<div class="winning-ticket">
 			<p style="margin:1rem; text-align: center">
-				22Bet<br /> <strong> Booking Code: </strong>
+				22BET<br /> <strong> Ghana Booking Code: </strong>
 			</p>
 			{#if !subscribed_value}
 				<img {src} alt="" on:click={handleModal} />
 			{/if}
 
 			{#if subscribed_value}
-				<h3 class="vipOdds">kBeJBH 👏🏽</h3>
+				<h3 class="vipOdds">yjpHCt 👏🏽</h3>
 			{/if}
 			<p style="color: #919191; margin:1rem; text-align: center">
 				subscription is valid until ticket is won
