fdb35c45d601f7e6a7495beebd5b8e68b30b7755
Joshua Ofosu <joeokat@gmail.com>
2024-08-25 22:30:18 +0100

    Update Ticket.svelte

--- src/lib/components/Ticket.svelte
+++ src/lib/components/Ticket.svelte
@@ -32,7 +32,7 @@
 			</div>
 		</div>
 		<div 
-style="background-color: green; padding: 4px 10px; border-radius: 24px; color: white;"> WON 🔥 </div>
+style="background: green; padding: 4px 10px; border-radius: 24px; color: white;"> WON 🔥 </div>
 	</div>
 
 	<div class="odds">
