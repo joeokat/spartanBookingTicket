ec0338399ee4a22cf0c3dfcb44791ec918c30ff6
Joshua Ofosu <joeokat@gmail.com>
2024-09-10 22:01:13 +0100

    Update Vip.svelte

--- src/lib/components/Vip.svelte
+++ src/lib/components/Vip.svelte
@@ -60,7 +60,7 @@
 	<div class="ads--content">
 		<img {src} alt="display pic" />
 		<div>
-			<p class="heading">Daily VIP Odds</p>
+			<p class="heading">Weekly VIP Odds</p>
 			<div class="sub--heading">
 				<p>Join Spartan+ on Telegram</p>
 			</div>
@@ -68,6 +68,6 @@
 	</div>
 
 	<a class="ads--btn" href="https://paystack.com/buy/vip-odds">
-		Subscribe Weekly
+		Subscribe Now
 	</a>
 </div>
